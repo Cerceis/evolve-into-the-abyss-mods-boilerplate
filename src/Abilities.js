ad01050fc613c46752d6c890d9da8b0139ef260d
Cerceis <48720021+Cerceis@users.noreply.github.com>
2022-11-01 14:56:13 +0900

    11-01

--- src/Abilities.js
+++ src/Abilities.js
@@ -1,29 +1,19 @@
 /**
+ * Settings
+ * SOURCE: Name of your mod.
+ */
+const SOURCE = "MY_MOD";
+/**
  * Add your abilities here.
  */
 export const Abilities = {
     "0000":{
         id: "0000",
-        name: "God Mode",
-        description: "10% to remaining hp will be added as physical damage.",
+        name: "My First Ability",
+        source: SOURCE,
+        description: "My first ability.",
         stackable: true,
         effects:{
-            "onAdded":[
-                async(player) => {
-                    player.modPhysicalDmg += Math.round(player.hp * .1);
-                }
-            ],
-            "onEvent": [
-                async(player) => {
-                    player.modPhysicalDmg -= Math.round(player.previous.hp[1] * .1);
-                    player.modPhysicalDmg += Math.round(player.previous.hp[0] * .1);
-                }
-            ],
-            "onRemoved":[
-                async(player) => {
-                    player.modPhysicalDmg -= Math.round(player.hp * .1);
-                }
-            ],
         }
     },
 }
